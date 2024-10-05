541ba22d8fca778e85d6b69d894ba10fc00faaa8
RAJESHBAMMIDI <RAJESHBAMMIDI@RAJESH>
2024-10-05 13:18:21 +0530

    Demo framework created by sireesha

---
+++ Demoframework0001/src/test/java/ContactTest/Tocfreatecontact.java
@@ -0,0 +1,10 @@
+package ContactTest;
+
+public class Tocfreatecontact {
+
+	public static void main(String[] args) {
+		System.out.println("Created contact successfully");
+
+	}
+
+}
---
+++ Demoframework0001/src/test/java/organizationTest/Tocreateorganization.java
@@ -0,0 +1,8 @@
+package organizationTest;
+
+public class Tocreateorganization {
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+}
